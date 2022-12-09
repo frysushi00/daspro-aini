7457b11dc7afdd2de5583dbdb4199a83cdfe433c
Aini Nurul Amri <118587045+frysushi00@users.noreply.github.com>
2022-12-10 01:14:49 +0700

    menambahkan file uts2.cs

---
+++ uts2.cs
@@ -0,0 +1,44 @@
+﻿//Nama  : Aini Nurul Amri
+//NIM   : 2207135967
+//Prodi : Teknik Informatika-A
+
+using System;
+namespace uts
+{
+    class noDua
+    {
+
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            bool bermain = true;
+            int min = 1;
+            int max = 100;
+            int tebakan;
+            int angkaMisteri=70;
+            int tebakanPemain;
+            
+            while(bermain){
+                tebakan = 0;
+                tebakanPemain = 0;
+                
+
+                while(tebakan != angkaMisteri){
+                    Console.Write("Tebak angka antara " +min+"-" +max+" : ");
+                    tebakan = Convert.ToInt32(Console.ReadLine());
+
+                    if(tebakan < angkaMisteri){
+                        Console.WriteLine("Salah. Nilai terlalu rendah.");
+                    }else if(tebakan > angkaMisteri){
+                        Console.WriteLine("Salah.   Nilai terlalu tinggi.");
+                    }
+                    tebakanPemain++;
+                    }
+
+                    Console.WriteLine("Anda benar!");
+                    Console.WriteLine("Bye...");
+                    break;
+                  }
+                }
+             }
+           }
