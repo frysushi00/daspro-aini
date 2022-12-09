03a05dadf32803b28ba5ed89a3315dfaa2c43d9a
Aini Nurul Amri <118587045+frysushi00@users.noreply.github.com>
2022-12-10 01:18:09 +0700

    menambahkan file uts5.cs

---
+++ uts5.cs
@@ -0,0 +1,126 @@
+﻿//Nama  : Aini Nurul Amri
+//NIM   : 2207135967
+//Prodi : Teknik Informatika-A
+
+
+using System;
+
+namespace uts
+{
+    class noLima
+    {
+        
+        
+        static char[] arr = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+        static int pemain = 1; 
+        static int pilihan; 
+        static int komputer = 0;
+        static void Main(string[] args)
+        {
+            do
+            {
+            
+                Console.Clear();
+                Console.WriteLine("pemain :X and Komputer :O");
+                
+                TicTacToe();
+                pilihan = int.Parse(Console.ReadLine());
+               
+                if (arr[pilihan] != 'X' && arr[pilihan] != 'O')
+                {
+                    if (pemain % 2 == 0) 
+                    {
+                        arr[pilihan] = 'O';
+                        pemain++;
+                    }
+                    else
+                    {
+                        arr[pilihan] = 'X';
+                        komputer++;
+                    }
+                }
+                else
+                komputer = CekKemenangan();
+            }
+            while (komputer != 1 && komputer != -1);
+            
+            Console.Clear();
+            TicTacToe();
+            
+        }
+        
+        private static void TicTacToe()
+        {
+             Console.WriteLine("     |     |      ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}", arr[1], arr[2], arr[3]);
+            Console.WriteLine("_____|_____|_____ ");
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}", arr[4], arr[5], arr[6]);
+            Console.WriteLine("_____|_____|_____ ");
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}", arr[7], arr[8], arr[9]);
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine("Pilih posisi lalu tekan Enter");
+        }
+        
+        private static int CekKemenangan()
+        {
+            #region cek horizontal
+            if (arr[1] == arr[2] && arr[2] == arr[3])
+            {
+                return 1;
+            }
+            
+            else if (arr[4] == arr[5] && arr[5] == arr[6])
+            {
+                return 1;
+            }
+           
+            else if (arr[6] == arr[7] && arr[7] == arr[8])
+            {
+                return 1;
+            }
+            #endregion
+            #region cek vertikal
+            else if (arr[1] == arr[4] && arr[4] == arr[7])
+            {
+                return 1;
+            }
+            
+            else if (arr[2] == arr[5] && arr[5] == arr[8])
+            {
+                return 1;
+            }
+            
+            else if (arr[3] == arr[6] && arr[6] == arr[9])
+            {
+                return 1;
+            }
+            #endregion
+            #region cek diagonal
+            else if (arr[1] == arr[5] && arr[5] == arr[9])
+            {
+                return 1;
+            }
+            else if (arr[3] == arr[5] && arr[5] == arr[7])
+            {
+                return 1;
+            }
+            #endregion
+            #region cek jika seri
+            
+            else if (arr[1] != '1' && arr[2] != '2' && arr[3] != '3' && arr[4] != '4' && arr[5] != '5' && arr[6] != '6' && arr[7] != '7' && arr[8] != '8' && arr[9] != '9')
+            {
+                return -1;
+            }
+            #endregion
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
+
+
+
