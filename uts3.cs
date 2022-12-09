9c27a1831a4b81c1066f86e7517185ee80d4c24c
Aini Nurul Amri <118587045+frysushi00@users.noreply.github.com>
2022-12-10 01:15:39 +0700

    menambhakan fiel uts3.cs

---
+++ uts3.cs
@@ -0,0 +1,36 @@
+﻿//Nama  : Aini Nurul Amri
+//NIM   : 2207135967
+//Prodi : Teknik Informatika-A
+
+using System;
+namespace uts{
+    class noTiga
+    
+    {
+        static void Main(string[] args){
+            Intro();
+        }
+        static void Intro(){
+                int TotalDenda = 0;
+
+                Console.Write("Input jumlah hari peminjaman : ");
+                int Haripeminjaman = Convert.ToInt32(Console.ReadLine());
+                if(Haripeminjaman <= 0 && Haripeminjaman <= 5){
+                    TotalDenda = 0;
+                }else if(Haripeminjaman > 5 && Haripeminjaman <= 10){
+                    Haripeminjaman -= 5;
+                    TotalDenda = Haripeminjaman*10000;
+                }else if (Haripeminjaman > 10 && Haripeminjaman <= 30){
+                    Haripeminjaman -= 10;
+                    TotalDenda = Haripeminjaman*20000+50000;
+                }else if(Haripeminjaman>30){
+                    Haripeminjaman -= 30;
+                    Console.WriteLine("Keanggotaan anda dibatalkan.");
+                    TotalDenda = Haripeminjaman*30000+50000+400000;
+                }else{
+                    
+            }
+            Console.WriteLine("Total Denda : Rp." + TotalDenda);
+        }
+    }
+}
